22b37a1dd73ef9098ee28604867cdba70daa73e0
Paul <me@paulhidalgo.net>
2018-02-13 10:37:09 +1100

    Nmap

--- all.sh
+++ all.sh
@@ -1,4 +1,4 @@
 sh ./ip-generator.sh >> /var/log/attackall &
 sh ./bruteforce-rdp.sh >> /var/log/attackall &
 sh ./bruteforce-ssh.sh >> /var/log/attackall &
-sh ./bruteforce-nmap.sh >> /var/log/attackall &
+sh ./nmap.sh >> /var/log/attackall &
