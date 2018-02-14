748d5efb0a5d5f9bdd3dd3a4bea224b634502be4
Paul <me@paulhidalgo.net>
2018-02-15 10:31:29 +1100

    Added Comments

--- all.sh
+++ all.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+echo "Running IP Generator"
 sh ./ip-generator.sh >> /var/log/attackall &
+
+echo "Running RDP Bruteforce"
 sh ./bruteforce-rdp.sh >> /var/log/attackall &
+
+echo "Running SSH Bruteforce"
 sh ./bruteforce-ssh.sh >> /var/log/attackall &
+
+echo "Running NMAP"
 sh ./nmap.sh >> /var/log/attackall &
